6282c880efb3963325655ab6804ae816d2d1b052
Mayank <mayank@scanbot.io>
2023-05-12 17:55:18 +0200

    - [x] Dumped the locally maintained Document scanner project

---
+++ ScanbotSDKMAUIExample/ScanbotSDKMAUIExample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DocumentSDK.MAUI.Example;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
---
+++ ScanbotSDKMAUIExample/ScanbotSDKMAUIExample/Utils/StringUtils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DocumentSDK.MAUI.Example.Utils
+{
+    public class StringUtils
+    {
+        public static string CopyrightLabel =>
+            $"Copyright (c) {DateTime.Now.Year} Scanbot SDK GmbH. All rights reserved";
+        
+    }
+}
