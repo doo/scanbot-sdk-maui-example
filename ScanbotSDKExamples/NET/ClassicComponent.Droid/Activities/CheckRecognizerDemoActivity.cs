22189739259560e0c7b8b48fece2bca3f90c3b1e
Mayank <mayank@scanbot.io>
2023-08-07 18:20:37 +0200

    - [x] Droid - Migrated Classical component Droid to NET

---
+++ ScanbotSDKExamples/NET/ClassicComponent.Droid/Activities/CheckRecognizerDemoActivity.cs
@@ -0,0 +1,92 @@
+﻿using Android.Content;
+using AndroidX.AppCompat.App;
+using ClassicalComponentsDemo.Droid;
+using ClassicComponent.Droid.Delegates;
+using IO.Scanbot.Sdk.Camera;
+using IO.Scanbot.Sdk.Check;
+using IO.Scanbot.Sdk.Check.Entity;
+using IO.Scanbot.Sdk.UI;
+using IO.Scanbot.Sdk.UI.Camera;
+
+namespace ClassicComponent.Droid.Activities
+{
+    [Activity(Theme = "@style/Theme.AppCompat")]
+    public class CheckRecognizerDemoActivity : AppCompatActivity
+    {
+        private ScanbotCameraXView cameraView;
+        private TextView resultView;
+        private CheckRecognizerFrameHandlerWrapper checkFrameHandlerWrapper;
+        private IO.Scanbot.Sdk.ScanbotSDK scanbotSDK;
+        private bool isFlashEnabled = false;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ActivityCheckRecognizer);
+
+            cameraView = FindViewById<ScanbotCameraXView>(Resource.Id.camera);
+            if (cameraView != null)
+            {
+                cameraView.SetPreviewMode(CameraPreviewMode.FitIn);
+                cameraView.SetCameraOpenCallback(new Delegates.CameraOpenCallback(cameraView, isFlashEnabled));
+            }
+
+            resultView = FindViewById<TextView>(Resource.Id.result);
+            scanbotSDK = new IO.Scanbot.Sdk.ScanbotSDK(this);
+            ICheckRecognizer checkScanner = scanbotSDK.CreateCheckRecognizer();
+            checkFrameHandlerWrapper = new CheckRecognizerFrameHandlerWrapper(checkScanner);
+            var checkResultHandler = new CheckRecognizerResultDelegate();
+            checkResultHandler.OnCheckRecognized += HandleCheckResult;
+            checkFrameHandlerWrapper.AddResultHandler(checkResultHandler);
+            ScanbotCameraXViewWrapper.Attach(cameraView, checkFrameHandlerWrapper);
+
+            FindViewById<Button>(Resource.Id.flash).SetOnClickListener(new OnClickListener(() =>
+            {
+                this.isFlashEnabled = !this.isFlashEnabled;
+                this.cameraView.UseFlash(this.isFlashEnabled);
+            }));
+
+            Toast.MakeText(
+                this,
+                scanbotSDK.IsLicenseActive ? "License is active" : "License Expired",
+                ToastLength.Long
+            ).Show();
+        }
+
+        private void CounterDetected(object sender, ContourDetectorEventArgs e)
+        {
+
+        }
+
+        private void HandleCheckResult(object sender, CheckRecognizerResult result)
+        {
+            if (result.Status == IO.Scanbot.Check.Model.CheckRecognizerStatus.Success)
+            {
+                this.checkFrameHandlerWrapper.FrameHandler.Enabled = false;
+                StartActivity(CheckRecognizerResultActivity.NewIntent(this, result));
+            }
+            else if (!this.scanbotSDK.IsLicenseActive)
+            {
+                this.checkFrameHandlerWrapper.FrameHandler.Enabled = false;
+                RunOnUiThread(() =>
+                {
+                    Toast.MakeText(this, "License is expired", ToastLength.Long).Show();
+                    Finish();
+                });
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (checkFrameHandlerWrapper?.FrameHandler != null)
+            {
+                this.checkFrameHandlerWrapper.FrameHandler.Enabled = true;
+            }
+        }
+
+        public static Intent NewIntent(Context context)
+        {
+            return new Intent(context, typeof(CheckRecognizerDemoActivity));
+        }
+    }
+}
---
+++ ScanbotSDKExamples/NET/ClassicComponent.Droid/Activities/CroppingImageDemoActivity.cs
@@ -0,0 +1,188 @@
+﻿using Android.Graphics;
+using Android.Views;
+using Android.Content;
+using Android.Util;
+
+using AndroidNetUri = Android.Net.Uri;
+using AndroidX.AppCompat.App;
+using IO.Scanbot.Sdk.UI;
+using IO.Scanbot.Sdk.Core.Contourdetector;
+using IO.Scanbot.Sdk.Process;
+using ClassicalComponentsDemo.Droid;
+
+namespace ClassicComponent.Droid
+{
+    [Activity(Theme = "@style/Theme.AppCompat")]
+    public class CroppingImageDemoActivity : AppCompatActivity
+    {
+        static string LOG_TAG = typeof(CroppingImageDemoActivity).Name;
+
+        public static String EXTRAS_ARG_IMAGE_FILE_URI = "EXTRAS_ARG_IMAGE_FILE_URI";
+
+        static IList<PointF> DEFAULT_POLYGON = new List<PointF>();
+
+        IO.Scanbot.Sdk.ScanbotSDK SDK;
+
+        static CroppingImageDemoActivity()
+        {
+            DEFAULT_POLYGON.Add(new PointF(0, 0));
+            DEFAULT_POLYGON.Add(new PointF(1, 0));
+            DEFAULT_POLYGON.Add(new PointF(1f, 1f));
+            DEFAULT_POLYGON.Add(new PointF(0, 1));
+        }
+
+        AndroidNetUri imageUri;
+        Bitmap originalBitmap;
+        EditPolygonImageView editPolygonImageView;
+        MagnifierView scanbotMagnifierView;
+        ProgressBar processImageProgressBar;
+        View cancelBtn, doneBtn, rotateCWButton;
+
+        int rotationDegrees = 0;
+        long lastRotationEventTs = 0L;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.CroppingImageDemo);
+
+            SupportActionBar.SetDisplayShowHomeEnabled(false);
+            SupportActionBar.SetDisplayShowTitleEnabled(false);
+            SupportActionBar.SetDisplayShowCustomEnabled(true);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            SupportActionBar.SetCustomView(Resource.Layout.CroppingActionBarView);
+
+            editPolygonImageView = FindViewById<EditPolygonImageView>(Resource.Id.scanbotEditImageView);
+            scanbotMagnifierView = FindViewById<MagnifierView>(Resource.Id.scanbotMagnifierView);
+            processImageProgressBar = FindViewById<ProgressBar>(Resource.Id.processImageProgressBar);
+
+            cancelBtn = FindViewById<View>(Resource.Id.cancelButton);
+            cancelBtn.Click += delegate
+            {
+                Finish();
+            };
+
+            doneBtn = FindViewById<View>(Resource.Id.doneButton);
+            doneBtn.Click += delegate
+            {
+                cancelBtn.Enabled = false;
+                doneBtn.Enabled = false;
+                rotateCWButton.Enabled = false;
+                CropAndSaveImage();
+            };
+
+            rotateCWButton = FindViewById<View>(Resource.Id.rotateCWButton);
+            rotateCWButton.Click += delegate
+            {
+                if ((Java.Lang.JavaSystem.CurrentTimeMillis() - lastRotationEventTs) < 350)
+                {
+                    return;
+                }
+                rotationDegrees += 90;
+                editPolygonImageView.RotateClockwise();
+                lastRotationEventTs = Java.Lang.JavaSystem.CurrentTimeMillis();
+            };
+
+            string imageFileUri = Intent.Extras.GetString(EXTRAS_ARG_IMAGE_FILE_URI);
+            imageUri = AndroidNetUri.Parse(imageFileUri);
+            InitImageView();
+
+            SDK = new IO.Scanbot.Sdk.ScanbotSDK(this);
+        }
+
+        void InitImageView()
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    var polygon = DEFAULT_POLYGON;
+
+                    originalBitmap = ImageLoader.Instance.Load(imageUri);
+                    Bitmap resizedBitmap = ImageUtils.ResizeImage(originalBitmap, 1000, 1000);
+
+                    RunOnUiThread(() =>
+                    {
+                        // important! first set the image and then the detected polygon and lines!
+                        editPolygonImageView.SetImageBitmap(resizedBitmap);
+                        // set up the MagnifierView every time when editPolygonView is set with a new image.
+                        scanbotMagnifierView.SetupMagnifier(editPolygonImageView);
+                    });
+
+                    var detector = SDK.CreateContourDetector();
+                    // Since we just need detected polygon and lines here, we use ContourDetector class from the native SDK namespace.
+                    var detectionResult = detector.Detect(resizedBitmap);
+                    var detectionStatus = detectionResult.Status;
+                    if (detectionStatus == DetectionStatus.Ok || detectionStatus == DetectionStatus.OkButBadAngles ||
+                        detectionStatus == DetectionStatus.OkButTooSmall || detectionStatus == DetectionStatus.OkButBadAspectRatio)
+                    {
+                        polygon = detectionResult.PolygonF;
+                        DebugLog("Detected polygon: " + polygon);
+                    }
+
+                    RunOnUiThread(() =>
+                    {
+                        editPolygonImageView.Polygon = polygon;
+                        editPolygonImageView.SetLines(detectionResult.HorizontalLines, detectionResult.VerticalLines);
+                    });
+
+                }
+                catch (Exception e)
+                {
+                    ErrorLog("Could not initialize image view", e);
+                }
+            });
+        }
+
+        void CropAndSaveImage()
+        {
+            processImageProgressBar.Visibility = ViewStates.Visible;
+            cancelBtn.Visibility = ViewStates.Gone;
+            doneBtn.Visibility = ViewStates.Gone;
+            rotateCWButton.Visibility = ViewStates.Gone;
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    var detector = SDK.CreateContourDetector();
+                    var documentImage = SDK.ImageProcessor().ProcessBitmap(originalBitmap, new CropOperation(editPolygonImageView.Polygon), false);
+                    documentImage = DocumentSDK.MAUI.Native.Droid.ScanbotSDK.RotateImage(documentImage, -rotationDegrees);
+                    var documentImgUri = MainApplication.TempImageStorage.AddImage(documentImage);
+
+                    RunOnUiThread(() =>
+                    {
+                        var extras = new Bundle();
+                        extras.PutString(EXTRAS_ARG_IMAGE_FILE_URI, documentImgUri.ToString());
+                        var intent = new Intent();
+                        intent.PutExtras(extras);
+                        SetResult(Result.Ok, intent);
+                        Finish();
+                    });
+                }
+                catch (Exception e)
+                {
+                    ErrorLog("Could not apply image changes", e);
+                }
+            });
+        }
+
+
+        void DebugLog(string msg)
+        {
+            Log.Debug(LOG_TAG, msg);
+        }
+
+        void ErrorLog(string msg)
+        {
+            Log.Error(LOG_TAG, msg);
+        }
+
+        void ErrorLog(string msg, Exception ex)
+        {
+            Log.Error(LOG_TAG, Java.Lang.Throwable.FromException(ex), msg);
+        }
+    }
+}
