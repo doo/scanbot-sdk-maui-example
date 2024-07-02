a705ea2ea7020f76e9b7fd76352e4a01665ac574
Matthew Rindel <matthew@scanbot.io>
2024-07-02 07:47:01 +0200

    net: updated snippets

--- NET/ReadyToUseUI.Droid/Snippets/ItemMapping.cs
+++ NET/ReadyToUseUI.Droid/Snippets/ItemMapping.cs
@@ -1,23 +1,32 @@
 using IO.Scanbot.Sdk.Ui_v2.Barcode.Configuration;
+using IO.Scanbot.Sdk.Ui_v2.Common;
 
-namespace ReadyToUseUI.Droid.Snippets;
+namespace ReadyToUseUI.Droid;
 
-public class ItemMappingConfigSnippet
+public static partial class Snippets
 {
-    public BarcodeScannerConfiguration GetItemMappingConfigSnippetConfiguration()
+    public static BarcodeScannerConfiguration ItemMapping
     {
-        var configuration = new BarcodeScannerConfiguration();
-        configuration.UseCase = new SingleScanningMode()
+        get
         {
-            BarcodeInfoMapping = new BarcodeInfoMapping()
+            // Create the default configuration object.
+            var config = new BarcodeScannerConfiguration();
+            
+            var useCase = new SingleScanningMode();
+
+            useCase.ConfirmationSheetEnabled = true;
+            useCase.BarcodeInfoMapping = new BarcodeInfoMapping()
             {
                 BarcodeItemMapper = new CustomMapper()
-            }
-        };
+            };
 
-        return configuration;
+            // Configure other parameters, pertaining to single-scanning mode as needed.
+            config.UseCase = useCase;
+
+            return config;
+        }
     }
-    
+
     public class CustomMapper : global::Java.Lang.Object, global::Java.IO.ISerializable, IO.Scanbot.Sdk.Ui_v2.Barcode.Configuration.IBarcodeItemMapper
     {
         public void MapBarcodeItem(BarcodeItem barcodeItem, IBarcodeMappingResult result)
@@ -32,7 +41,7 @@ public class ItemMappingConfigSnippet
             }
             else
             {
-                result.OnResult(new BarcodeMappedData(barcodeImage: image, title: title, subtitle: subTitle));
+                result.OnResult(new BarcodeMappedData(title: title, subtitle: subTitle, barcodeImage: image));
             }
         }
     }
