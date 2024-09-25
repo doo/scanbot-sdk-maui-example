0ed1a2fde479eef6f04600ceee31147b175704bd
Mayank <mayank@scanbot.io>
2024-09-25 09:03:17 +0200

    MAUI Classic Doc: - Added new Document Scanner example page and lisiting page for displaying all items in example.

---
+++ MAUI/ClassicComponent.Maui/Pages/DocumentScannerResultPage.xaml.cs
@@ -0,0 +1,22 @@
+namespace ClassicComponent.Maui.Pages;
+
+public partial class DocumentScannerResultPage : ContentPage
+{
+	private ImageSource _imageSource;
+	
+	internal void NavigateData(ImageSource source)
+	{
+		_imageSource = source;
+	}
+	
+	public DocumentScannerResultPage()
+	{
+		InitializeComponent();
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		imagePreview.Source = _imageSource;
+	}
+}
