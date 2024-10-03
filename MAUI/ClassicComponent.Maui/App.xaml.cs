8b10a3a59815328536090a0950b5d91378f99c8c
Mayank <mayank@scanbot.io>
2024-10-03 17:24:17 +0200

    MAUI Classic Document: - Added Document Scanner Classic Component full example with Binding properties. - Moved the Scanners in corresponding folders - Added a home page for scanner listing - Deleted the previous MainPage

--- MAUI/ClassicComponent.Maui/App.xaml.cs
+++ MAUI/ClassicComponent.Maui/App.xaml.cs
@@ -7,7 +7,7 @@
         public App()
         {
             InitializeComponent();
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new HomePage());
         }
     }
 }
