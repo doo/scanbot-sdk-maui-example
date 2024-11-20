79b7193ecb5a0e1107d2d32d03a9c774fc3d5c9f
Mayank <mayank@scanbot.io>
2024-11-20 21:13:06 +0100

    MAUI Classic DocScanner: - Added a new Scanning Items lisiting page for displaying all items in example. - Added Document Scanner Classic Component full example with Binding properties. - Moved the Scanners in their respective folders - Deleted the previous MainPage and related code. - Added a new page for Barcode Classic Component example and a separate page for Custom implementation for Barcode Classic component example.

---
+++ MAUI/ClassicComponent.Maui/Pages/BarcodeScanner/ClassicBarcodeScannerPage.xaml.cs
@@ -0,0 +1,75 @@
+using ScanbotSDK.MAUI.Configurations;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using ScanbotSDK.MAUI;
+using ScanbotSDK.MAUI.Common;
+using ScanbotSDK.MAUI.RTU.v1;
+
+namespace ClassicComponent.Maui;
+
+public partial class ClassicBarcodeScannerPage : ContentPage
+
+{
+	public ClassicBarcodeScannerPage()
+	{
+		InitializeComponent();
+
+		cameraView.OverlayConfiguration = new SelectionOverlayConfiguration(true, BarcodeTextFormat.CodeAndType,
+							textColor: Colors.Yellow,
+							textContainerColor: Colors.Black,
+							strokeColor: Colors.Yellow,
+							highlightedTextColor: Colors.Red,
+							highlightedTextContainerColor: Colors.Black,
+							highlightedStrokeColor: Colors.Red);
+
+		cameraView.FinderConfiguration = new FinderConfiguration
+		{
+							IsFinderEnabled = true,
+							FinderLineColor = Colors.WhiteSmoke,
+							FinderOverlayColor = Colors.Black.WithAlpha(0.5f),
+							FinderLineCornerRadius = 20,
+							FinderLineWidth = 5,
+							FinderAspectRatio = new AspectRatio(1,1)
+		};
+	}
+
+	private void HandleScannerResults(BarcodeResultBundle result)
+	{
+		string text = string.Empty;
+
+		if (result?.Barcodes != null)
+		{
+			foreach (var barcode in result.Barcodes)
+			{
+				text += $"{barcode.Text} ({barcode.Format.ToString().ToUpper()})\n";
+				text += "--------------------------\n";
+			}
+		}
+
+		System.Diagnostics.Debug.WriteLine(text);
+		lblResult.Text = text;
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+
+		// Start barcode detection manually
+		cameraView.StartDetection();
+	}
+
+	protected override void OnDisappearing()
+	{
+		base.OnDisappearing();
+
+		// Stop barcode detection manually
+		cameraView.StopDetection();
+
+		cameraView.Handler?.DisconnectHandler();
+	}
+
+	private void CameraView_OnOnBarcodeScanResult(BarcodeResultBundle result)
+	{
+		HandleScannerResults(result);
+	}
+}
