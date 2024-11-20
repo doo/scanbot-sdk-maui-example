8c4c7146b3f92448fa33df2a9541855336081f36
Mayank <mayank@scanbot.io>
2024-11-20 20:30:36 +0100

    NET 6.0 SDK: Added new Snippets for Docs

---
+++ NET/ReadyToUseUI.Droid/Snippets/TIFFSnippet.cs
@@ -0,0 +1,42 @@
+using AndroidX.AppCompat.App;
+using IO.Scanbot.Sdk.Docprocessing;
+using IO.Scanbot.Sdk.Imagefilters;
+using IO.Scanbot.Sdk.Tiff.Model;
+
+namespace ReadyToUseUI.Droid.Snippets;
+
+public class TIFFSnippet : AppCompatActivity
+{
+	private IO.Scanbot.Sdk.ScanbotSDK _scanbotSdk;
+
+	protected override void OnCreate(Bundle savedInstanceState)
+	{
+		base.OnCreate(savedInstanceState);
+
+		// Returns the singleton instance of the Sdk.
+		_scanbotSdk = new IO.Scanbot.Sdk.ScanbotSDK(this);
+		var document = _scanbotSdk.DocumentApi.LoadDocument("Your_Document_Id");
+		
+		if (_scanbotSdk.LicenseInfo.IsValid)
+		{
+			CreateTiffFromDocument(document);
+		}
+	}
+
+	private void CreateTiffFromDocument(Document document)
+	{
+		var tiffFile = new Java.IO.File(document?.TiffUri?.Path ?? "");
+		
+		var options = new IO.Scanbot.Sdk.Tiff.Model.TIFFImageWriterParameters(
+							binarizationFilter: new ScanbotBinarizationFilter(),
+							dpi:200,
+							compression: IO.Scanbot.Sdk.Tiff.Model.TIFFImageWriterCompressionOptions.CompressionNone,
+							userDefinedFields: Array.Empty<TIFFImageWriterUserDefinedField>());
+
+		var isTiffGenerated = _scanbotSdk.CreateTiffWriter().WriteTIFF(document, tiffFile, options);
+		if (isTiffGenerated && document?.TiffUri != null)
+		{
+			// Do something with the TIFF file
+		}
+	}
+}
