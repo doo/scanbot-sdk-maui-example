b4a1e4952ef1acbc731e3b49f77e607a7e1780a0
Mayank <mayank@scanbot.io>
2025-07-08 17:25:22 +0200

    DS MAUI 7.0: Fixed Snippets Indentation

--- MAUI/ScanbotSdkExample.Maui/Snippets/DocumentScanner/LaunchSnippet.cs
+++ MAUI/ScanbotSdkExample.Maui/Snippets/DocumentScanner/LaunchSnippet.cs
@@ -5,19 +5,19 @@ namespace ScanbotSdkExample.Maui.Snippets.DocumentScanner;
 
 public static class LaunchSnippet
 {
-	private static async Task LaunchAsync()
-	{
-		// Create the default configuration object.
-		var configuration = new DocumentScanningFlow();
+    private static async Task LaunchAsync()
+    {
+        // Create the default configuration object.
+        var configuration = new DocumentScanningFlow();
 
-		try
-		{
-			var document = await ScanbotSDKMain.Rtu.DocumentScanner.LaunchAsync(configuration);
-			// Handle the document.
-		}
-		catch (TaskCanceledException)
-		{
-			// Indicates that the cancel button was tapped.
-		}
-	}
+        try
+        {
+            var document = await ScanbotSDKMain.Rtu.DocumentScanner.LaunchAsync(configuration);
+            // Handle the document.
+        }
+        catch (TaskCanceledException)
+        {
+            // Indicates that the cancel button was tapped.
+        }
+    }
 }
--- MAUI/ScanbotSdkExample.Maui/Snippets/DocumentScanner/MultiPageSnippet.cs
+++ MAUI/ScanbotSdkExample.Maui/Snippets/DocumentScanner/MultiPageSnippet.cs
@@ -5,25 +5,25 @@ namespace ScanbotSdkExample.Maui.Snippets.DocumentScanner;
 
 public static class MultiPageSnippet
 {
-	private static async Task LaunchAsync()
-	{
-		// Create the default configuration object.
-		var configuration = new DocumentScanningFlow();
+    private static async Task LaunchAsync()
+    {
+        // Create the default configuration object.
+        var configuration = new DocumentScanningFlow();
 
-		// Set the page limit to 0, to disable the limit, or set it to the number of pages you want to scan.
-		configuration.OutputSettings.PagesScanLimit = 0;
+        // Set the page limit to 0, to disable the limit, or set it to the number of pages you want to scan.
+        configuration.OutputSettings.PagesScanLimit = 0;
 
-		// Disable the acknowledgment screen.
-		configuration.Screens.Camera.Acknowledgement.AcknowledgementMode = AcknowledgementMode.None;
+        // Disable the acknowledgment screen.
+        configuration.Screens.Camera.Acknowledgement.AcknowledgementMode = AcknowledgementMode.None;
 
-		try
-		{
-			var document = await ScanbotSDKMain.Rtu.DocumentScanner.LaunchAsync(configuration);
-			// Handle the document.
-		}
-		catch (TaskCanceledException)
-		{
-			// Indicates that the cancel button was tapped.
-		}
-	}
+        try
+        {
+            var document = await ScanbotSDKMain.Rtu.DocumentScanner.LaunchAsync(configuration);
+            // Handle the document.
+        }
+        catch (TaskCanceledException)
+        {
+            // Indicates that the cancel button was tapped.
+        }
+    }
 }
